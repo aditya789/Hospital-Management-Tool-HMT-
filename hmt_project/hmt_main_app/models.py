8342a72fca1a1d527900965b0ae17f25acbb3ac8
unknown <aditya.shanmuk@gmail.com>
2024-02-23 15:30:18 +0530

    Third commit

--- hmt_project/hmt_main_app/models.py
+++ hmt_project/hmt_main_app/models.py
@@ -4,8 +4,30 @@ from django.db import models
 class HospitalDetail(models.Model):
     
     hospital_name = models.CharField(max_length =200,blank=True,default=None)
-    
+    about_us = models.TextField(blank=True,default=None)
+    learn_more = models.TextField(blank=True,default=None)
+    specialization = models.TextField(blank=True,default=None)
+    email_address = models.EmailField(blank=True,default=None)
+    contact_number = models.IntegerField(blank=True,default=None)
+    contact_address = models.TextField(blank=True,default=None)
+    opening_hours = models.TextField(blank=True,default=None)
+    hospital_rooms = models.IntegerField(blank=True,default=None)
+    established_on = models.DateField(blank=True,default=None)
+    promotions = models.TextField(blank=True,default=None)
     
     
     def __str__(self):
         return self.hospital_name
+    
+class ServiceDetail(models.Model):
+    
+    service_name = models.CharField(max_length=100,default=None,blank=True)
+    description = models.TextField(blank=True, default=None)
+    fee = models.IntegerField(blank=True,default=None)
+    
+class DoctorsDetail(models.Model):
+    
+    doctor_name=  models.CharField(max_length=100,default=None,blank=True)
+    qualification = models.CharField(max_length=100,default=None,blank=True)
+    specialization = models.CharField(max_length=100,default=None,blank=True)
+    available_timings = models.TextField(default=None,blank=True)
--- hmt_project/hmt_project/settings.py
+++ hmt_project/hmt_project/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 TEMPLATES_DIR = os.path.join(BASE_DIR, "Templates")
-STATIC_DIRS = os.path.join(BASE_DIR, "Static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -117,9 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
